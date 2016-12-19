d69f4afeac968aebe61f023cc23472a5e4323e59
naveedkumbhar <naveed_kumbhar@hotmail.com>
2016-12-20 01:40:30 +0500

    Added incremental backup script

---
+++ incremental-backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Purpose = Backup of Important Data
+#Created on 20/12/2016
+#Author = Naveed Ahmed
+#Version 1.0
+#START
+TIME=`date -I`          # This Command will add date in Backup File Name.
+FILENAME=backup-$TIME.tar.gz     # Here i define Backup file name format.
+SRCDIR=/var/www/                  # Location of Important Data Directory (Source of backup).
+DESDIR=/root/backup                   # Destination of backup file.
+SNF=/var/www/mydata.snap          # Snapshot file name and location
+tar -cvf $DESDIR/$FILENAME -g $SNF $SRCDIR  #Backup Command
+s3cmd put /root/backup/$FILENAME s3://s3-backup/
+#END
+
+
