a030e254ba8b9a9dcee7a9ec3fee6ecf7b2a212f
Naveed Ahmed <naveed_kumbhar@hotmail.com>
2016-12-21 14:56:13 +0500

    Update incremental-backup.sh
    Added monthly full backup string

--- incremental-backup.sh
+++ incremental-backup.sh
@@ -4,13 +4,12 @@
 #Author = Naveed Ahmed
 #Version 1.0
 #START
+MONTH=`date +%B`
 TIME=`date -I`          # This Command will add date in Backup File Name.
 FILENAME=backup-$TIME.tar.gz     # Here i define Backup file name format.
 SRCDIR=/var/www/                  # Location of Important Data Directory (Source of backup).
 DESDIR=/root/backup                   # Destination of backup file.
-SNF=/var/www/mydata.snap          # Snapshot file name and location
-tar -cvf $DESDIR/$FILENAME -g $SNF $SRCDIR  #Backup Command
-s3cmd put /root/backup/$FILENAME s3://s3-backup/
+SNF=/var/www/$MONTH          # Snapshot file name and location
+tar -cvf $DESDIR/$FILENAME -g $SNF.snap $SRCDIR  #Backup Command
+s3cmd put /root/backup/$FILENAME s3://s3-bucketname/
 #END
-
-
