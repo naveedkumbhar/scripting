edbb2eea50f9633455bb2df7359647bb6b713210
Naveed Ahmed <naveed.ahmed@cloudways.com>
2024-07-16 14:06:02 +0500

    added more descrption

--- drivesize.sh
+++ drivesize.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-#ScriptName: DirectorySize
-#Author: Naveed Ahmed
-#Company: PredictifyMe
+# ScriptName: DirectorySize
+# Author: Naveed Ahmed
+# Company: PredictifyMe
+# Description: This script calculates and displays the total size of a specified directory.
 
-echo "Enter the directory you want to know the size: "
+# Prompt the user to enter the directory they want to check
+echo "Please enter the directory path you want to check the size of:"
 read dir
-size="`du -ch $dir | grep total | awk '{print $1}'`"
-echo "Total size of $dir is $size"
 
+# Check if the directory exists
+if [ -d "$dir" ]; then
+    # Calculate the total size of the directory
+    size=$(du -ch "$dir" | grep total | awk '{print $1}')
+
+    # Display the total size to the user
+    echo "The total size of the directory '$dir' is $size."
+else
+    # Display an error message if the directory does not exist
+    echo "The directory '$dir' does not exist. Please enter a valid directory path."
+fi
 
