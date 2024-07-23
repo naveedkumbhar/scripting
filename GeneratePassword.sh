dc125a26902b9249eb265e20adf66ac7e7b1e774
naveedkumbhar <naveed_kumbhar@hotmail.com>
2024-07-23 13:10:04 +0500

    Updated GeneratePassword script to add conditions for password length

--- GeneratePassword.sh
+++ GeneratePassword.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-# Length of the password
-LENGTH=12
+# Function to display help
+display_help() {
+    echo "Usage: $0 [length]"
+    echo
+    echo "Generate a random password."
+    echo
+    echo "  length    specify the length of the password to generate (default is 12)"
+    echo
+}
+
+# Check if an argument is provided
+if [ $# -eq 0 ]; then
+    display_help
+    exit 1
+fi
+
+# Check if the argument is a valid number
+if ! [[ $1 =~ ^[0-9]+$ ]]; then
+    echo "Error: Length must be a number."
+    display_help
+    exit 1
+fi
+
+# Length of the password from the argument
+LENGTH=$1
 
 # Generate a random password
 PASSWORD=$(openssl rand -base64 $LENGTH)
