5057442731799db58479cc2966666f7ba7994da1
naveedkumbhar <naveed_kumbhar@hotmail.com>
2024-07-25 13:39:22 +0500

    Added Notification

---
+++ NotificationSend.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Install required package by uncommenting next line
+# sudo apt-get install libnotify-bin
+
+# Display a custom notification message
+notify-send "Reminder" "To-Do: Update GitHub repos \n\n1. github-workflows\n2. vagrantfiles\n3. scripting"
+
